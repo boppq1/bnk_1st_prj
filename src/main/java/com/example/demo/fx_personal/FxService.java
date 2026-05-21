f545e59772317579dcd9a0afc5ab53e635ab0e8f
hoony6555 <misohoon6555@gmail.com>
2026-05-21 15:03:21 +0900

    3시 3분 커밋

--- src/main/java/com/example/demo/fx_personal/FxService.java
+++ src/main/java/com/example/demo/fx_personal/FxService.java
@@ -1,10 +1,14 @@
 package com.example.demo.fx_personal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class FxService {
+	
+	@Autowired
+	FxDataDao fxDao;
 
     private final RestTemplate restTemplate = new RestTemplate();
     private final String auth_key = 
@@ -16,6 +20,13 @@ public class FxService {
         String url = "https://oapi.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=" + auth_key + "&searchdate=" + searchdate + "&data=AP01";
 
         return restTemplate.getForObject(url, String.class);
+    }
+    
+    public FxDataDto getFxData(String cur_nm) {
+    	
+    	FxDataDto fxData = fxDao.viewData(cur_nm);
+    	
+    	return fxData;
     }
 
 }
